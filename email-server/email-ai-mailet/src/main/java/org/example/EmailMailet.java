c506408515501507ea0d57844af050808bb583bc
Emma <enc4@students.waikato.ac.nz>
2023-06-01 09:19:57 +1200

    adds header to message

--- email-server/email-ai-mailet/src/main/java/org/example/EmailMailet.java
+++ email-server/email-ai-mailet/src/main/java/org/example/EmailMailet.java
@@ -1,20 +1,78 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.Optional;
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimePart;
 
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMailet;
+import org.apache.mailet.base.RFC2822Headers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.ibm.icu.lang.UCharacter.LineBreak.CARRIAGE_RETURN;
+
 public class EmailMailet extends GenericMailet{
-    private static final Logger logger = LoggerFactory.getLogger(EmailMailet.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailMailet.class);
+
     @Override
     public void service(Mail mail) throws MessagingException {
-        log("Email-AI barbarbar: log via mailet logger with simple log INFO level");
-        logger.info("Email-AI barbarbar: Log via slf4j with INFO level !!! Add log4j.logger.com.test=INFO, CONS, FILE in the log4j.properties");
-        logger.debug("Email-AI barbarbar: Log via slf4j with DEBUG level !!! Add log4j.logger.com.test=DEBUG, CONS, FILE in the log4j.properties");
-	logger.error("Email-AI foofoofoo: Log via slf4j with ERROR level !!! Add log4j.logger.com.test=DEBUG, CONS, FILE in the log4j.properties");
+        try {
+            MimeMessage message = mail.getMessage();
+
+            if (attachTLDR(message)) {
+                message.saveChanges();
+            } else {
+                LOGGER.info("Unable to add footer to mail {}", mail.getName());
+            }
+        } catch (IOException ioe) {
+            throw new MessagingException("Could not read message", ioe);
+        }
     }
+
+    private boolean attachTLDR(MimePart part) throws IOException, MessagingException {
+        String contentType = part.getContentType();
+
+        if (!(part.getContent() instanceof String)) {
+            LOGGER.info("was not string! bad! " + contentType);
+            return false;
+        }
+        if (part.getContent() instanceof String) {
+            Optional<String> content = attachTLDRToTextPart(part);
+            if (content.isPresent()) {
+                part.setContent(content.get(), contentType);
+                part.setHeader(RFC2822Headers.CONTENT_TYPE, contentType);
+                return true;
+            }
+
+        }
+        //Give up... we won't attach the footer to this MimePart
+        return false;
+
+    }
+    private Optional<String> attachTLDRToTextPart(MimePart part) throws MessagingException, IOException {
+        String content = (String) part.getContent();
+        if (part.isMimeType("text/plain")) {
+            return Optional.of(attachTLDRToText(content));
+        }
+        else{
+            LOGGER.info("not plain text! "+ part.getContentType());
+        }
+        return Optional.empty();
+    }
+
+    private String attachTLDRToText(String content) throws MessagingException,
+            IOException {
+        StringBuilder builder = new StringBuilder("hi from emma");
+        builder.append(CARRIAGE_RETURN);
+        builder.append(CARRIAGE_RETURN);
+        builder.append(content);
+        return builder.toString();
+    }
+
 
 }
