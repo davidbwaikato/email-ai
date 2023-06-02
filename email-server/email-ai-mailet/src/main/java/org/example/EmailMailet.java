fd1aeceb2dc05b4e63dadb13f61a0e7316c218d3
Emma_ <enc4@students.waikto.ac.nz>
2023-06-03 09:19:43 +1200

    supports multipart messages

--- email-server/email-ai-mailet/src/main/java/org/example/EmailMailet.java
+++ email-server/email-ai-mailet/src/main/java/org/example/EmailMailet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Optional;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.internet.MimePart;
@@ -37,11 +38,8 @@ public class EmailMailet extends GenericMailet{
     private boolean attachTLDR(MimePart part) throws IOException, MessagingException {
         String contentType = part.getContentType();
 
-        if (!(part.getContent() instanceof String)) {
-            LOGGER.info("was not string! bad! " + contentType);
-            return false;
-        }
-        if (part.getContent() instanceof String) {
+
+       if (part.getContent() instanceof String) {
             Optional<String> content = attachTLDRToTextPart(part);
             if (content.isPresent()) {
                 part.setContent(content.get(), contentType);
@@ -49,6 +47,23 @@ public class EmailMailet extends GenericMailet{
                 return true;
             }
 
+        }
+        if (part.isMimeType("multipart/mixed")
+                || part.isMimeType("multipart/related")) {
+            MimeMultipart multipart = (MimeMultipart) part.getContent();
+            boolean added = attachTLDRToFirstPart(multipart);
+            if (added) {
+                part.setContent(multipart);
+            }
+            return added;
+
+        } else if (part.isMimeType("multipart/alternative")) {
+            MimeMultipart multipart = (MimeMultipart) part.getContent();
+            boolean added = attachTLDRToAllSubparts(multipart);
+            if (added) {
+                part.setContent(multipart);
+            }
+            return added;
         }
         //Give up... we won't attach the footer to this MimePart
         return false;
@@ -72,6 +87,22 @@ public class EmailMailet extends GenericMailet{
         builder.append(CARRIAGE_RETURN);
         builder.append(content);
         return builder.toString();
+    }
+
+
+    private boolean attachTLDRToFirstPart(MimeMultipart multipart) throws MessagingException, IOException {
+        MimeBodyPart firstPart = (MimeBodyPart) multipart.getBodyPart(0);
+        return attachTLDR(firstPart);
+    }
+
+    private boolean attachTLDRToAllSubparts(MimeMultipart multipart) throws MessagingException, IOException {
+        int count = multipart.getCount();
+        boolean isFooterAttached = false;
+        for (int index = 0; index < count; index++) {
+            MimeBodyPart mimeBodyPart = (MimeBodyPart) multipart.getBodyPart(index);
+            isFooterAttached |= attachTLDR(mimeBodyPart);
+        }
+        return isFooterAttached;
     }
 
 
