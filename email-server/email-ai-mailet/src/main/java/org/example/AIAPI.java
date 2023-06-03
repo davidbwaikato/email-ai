c38c03d40fae5367623569e6e2044840e9cfa0b3
Emma_ <enc4@students.waikto.ac.nz>
2023-06-04 11:10:40 +1200

    testing response

--- email-server/email-ai-mailet/src/main/java/org/example/AIAPI.java
+++ email-server/email-ai-mailet/src/main/java/org/example/AIAPI.java
@@ -2,6 +2,8 @@ package org.example;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URLEncoder;
@@ -11,10 +13,14 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 public class AIAPI {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AIAPI.class);
+
     public String getTLDR(String text) throws Exception {
         //http://email-ai.interactwith.us/api-enc4/tldr
         Gson gson =  new Gson();
         String response = getTLDRJson(text);
+        LOGGER.info("response was " + response);
         TLDRResponse tldrResponse = gson.fromJson(response, TLDRResponse.class);
         StringBuilder builder = new StringBuilder();
         for (TLDRText tldrText: tldrResponse.info.text) {
