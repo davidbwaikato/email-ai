d3c8de86cf70190269eac40186ed7d18c4c654fd
David Bainbridge <davidb@waikato.ac.nz>
2023-06-03 23:01:08 +0000

    Development of a Maillet that monitors email messages, appending in some additiona text ot the subject line as demonstration of changes email messages handled by James

--- email-server/email-ai-mailet/src/main/java/org/emailai/MonitoringMailet.java
+++ email-server/email-ai-mailet/src/main/java/org/emailai/MonitoringMailet.java
@@ -1,8 +1,12 @@
 package org.emailai;
 
+import java.io.IOException;
 import java.util.Iterator;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeBodyPart;
 
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMailet;
@@ -20,15 +24,69 @@ public class MonitoringMailet extends GenericMailet
     public void service(Mail mail) throws MessagingException {
 
 	MimeMessage message = mail.getMessage();
-        String subject = message.getSubject();
+        String old_subject = message.getSubject();
+	String new_subject = old_subject + " Email-AI appended subject line bar bar bar";
 
-        logger.info("Email-AI MonitoringMailet: Log via slf4j with INFO level with Subject Line: " + subject);
+	message.setSubject(new_subject);
+		
+        logger.info("Email-AI MonitoringMailet: Log via slf4j with INFO level with Subject Line: " + new_subject);
 
-
-	message.setSubject(subject + " bar bar bar");
-	
+	// The following call is untested, and so for now  have commented it out
+	//addKeywordsToMail(mail);
     }
 
+
+    protected String getOpenAIKeywords(Mail mail)
+    {
+	// Hardwired example of adding in keywords for now
+	//
+	// The future step up here would be to access the text of
+	// the email message, send it to OpenAI via a URL call to the web-server-api.js
+	// and the set the 'keywords' variable to be what was returned
+	
+	String keywords = "Keywords: Important, Urgent";
+
+	return keywords;
+    }
+
+    protected void addKeywordsToMail(Mail mail){
+	
+	logger.info("Email-AI MonitoringMailet: Adding Keywords to start of email body");
+
+	try {
+	    
+	    MimeMessage message = mail.getMessage();
+	    
+	    // Get the existing content of the message
+	    Object content = message.getContent();
+	    
+	    // Create a new MimeMultipart to hold the modified content
+	    MimeMultipart modifiedContent = new MimeMultipart();
+	    
+	    // Create a MimeBodyPart for the keywords
+	    MimeBodyPart keywordPart = new MimeBodyPart();
+	    String keywords = getOpenAIKeywords(mail);
+	    keywordPart.setText("Keywords: " + keywords);
+	    
+	    // Add the keywords part to the modified content
+	    modifiedContent.addBodyPart(keywordPart);
+	    
+	    // Add the existing content to the modified content
+	    MimeBodyPart existingContentPart = new MimeBodyPart();
+	    existingContentPart.setContent(content, message.getContentType());
+	    modifiedContent.addBodyPart(existingContentPart);
+	    
+	    // Set the modified content as the new content of the message
+	    message.setContent(modifiedContent);
+	    
+	    // Update the Mail object with the modified message
+	    mail.setMessage(message);
+	}
+	catch (Exception e) {
+	    logger.error("Email-AI MonitoringMailet: An exception was through in addKeywordsToMail");		
+	    e.printStackTrace();
+	}
+    }
 }
 
 
